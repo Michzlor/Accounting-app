3d8ec6d2836cae4e8cde60b7d8d8bcccb63370d6
Michal Lorenc <Mlorenc11@protonmail.com>
2023-03-17 12:24:29 +0100

    W klasie Manager zaimplementowano metody: *assign - pozwala na przypisanie funkcji programu do komend tekstowych *execute - wykonuje funkcje przypisne do komend tekstowych

--- accounting.py
+++ accounting.py
@@ -1,47 +1,53 @@
 with open("balance.txt", "r") as f:
-    acc_val = float(f.readline())
+    impport_acc_val = float(f.readline())
 with open("stock_file.txt", "r") as f:
-    stock = []
+    import_stock = []
     read = f.readlines()
     for i in range(0, len(read), 3):
         try:
             item = [read[i].strip(), (float(read[i+1].strip())), (int(read[i+2].strip()))]
-            stock.append(item)
+            import_stock.append(item)
         except Exception as e:
             print(e)
             break
-stock.append([None])
+import_stock.append([None])
 class Manager:
-    acc_val = acc_val
+
     def __init__(self):
         self.actions = {}
-        self.acc_val = acc_val
-        self.stock = stock
+        self.acc_val = impport_acc_val
+        self.stock = import_stock
+        self.audit = []
     def assign(self, name):
         def decorate(cb):
             self.actions[name] = cb
 
         return decorate
 
-    def execute(self, name):
+    def execute(self, name, *args, **kwarg):
         if name not in self.actions:
             print("Action not defined")
         else:
-            self.actions[name](self)
+            return self.actions[name](self, *args, **kwarg)
+
 
 manager = Manager()
 @manager.assign("balance")
-def balance(acc_val):
+def balance(manager, acc_val, audit_log):
     val = float(input("Provide value to add to account(negative value to substract): "))
+
     if acc_val + val < 0:
         print("Debt not allowed\n\n")
     else:
         acc_val += val
+        manager.acc_val = acc_val
         print("New account balance is:", round(acc_val, 2), "$")
-    balance = (val, acc_val)
-    audit.append(val)
 
-def sale(acc_val):
+    balance = (val, acc_val)
+    audit_log.append(balance)
+    return acc_val
+@manager.assign("sale")
+def sale(manager, acc_val, stock, audit_log):
     item = input("Input item: ")
     price = float(input("Input price: "))
     ammount = int(input("Input ammount: "))
@@ -55,15 +61,15 @@ def sale(acc_val):
             if stock[idx][2] < ammount:
                 print("Insufficent stock to make sale\n\n")
             else:
-                stock[idx][2] -= ammount
-                acc_val += price * ammount
+                manager.stock[idx][2] -= ammount
+                manager.acc_val += price * ammount
                 break
         elif stock[idx][0] == None or stock[idx][2] == 0:
             print("\n\nItem not in stock")
     sale = ("sale", item, price, ammount)
-    audit.append(sale)
-
-def buy(acc_val):
+    audit_log.append(sale)
+@manager.assign("buy")
+def buy(manager, acc_val, stock, audit_log):
     item = input("Input item: ")
     price = float(input("Input price: "))
     ammount = int(input("Input ammount: "))
@@ -83,15 +89,16 @@ def buy(acc_val):
             acc_val -= price * ammount
             break
     sale = ("buy", item, price, ammount)
-    audit.append(sale)
-def print_acc_val(acc_val):
+    audit_log.append(sale)
+@manager.assign("account")
+def print_acc_val(manager, acc_val):
     print("Account balance is:", round(acc_val, 2), "$")
-
-def list_stock(stock):
+@manager.assign("stock")
+def list_stock(manager, stock):
     for idx in range(len(stock) - 1):
         print("Item:{}  ;  Price:{}$  ;  Ammount:{}".format(stock[idx][0], stock[idx][1], stock[idx][2]))
-
-def list_item():
+@manager.assign("item")
+def list_item(manager, stock):
     inp = input("Input item ")
     for idx in range(len(stock)):
         if stock[idx][0] == inp:
@@ -99,8 +106,8 @@ def list_item():
             break
         elif stock[idx][0] == None:
             print("Not in stock\n\n")
-
-def audit_func():
+@manager.assign("audit")
+def audit_func(manager, audit_log):
     start = input("Insert start of range: ")
     end = input("Insert end of range: ")
     if start == "":
@@ -108,14 +115,14 @@ def audit_func():
     else:
         idx = int(start) - 1
     if end == "":
-        end = len(audit)
+        end = len(audit_log)
     else:
         end = (int(end)) - 1
     while idx < end:
-        print(audit[idx])
+        print(audit_log[idx])
         idx += 1
-
-def exit():
+@manager.assign("exit")
+def exit(manager, acc_val, stock, audit):
     print("\n\nHave a nice day\n\n")
     print(audit)
     with open("log.txt", "a") as log:
@@ -137,14 +144,7 @@ def exit():
         f.write("\n")
 
 
-item = True
-
-
-audit = []
-
-i = 1
-manager.execute("balance")
-while i == 1:
+while True:
 #0-lista+input
     print("*Balance(1)\n"
           "*Sale(2)\n"
@@ -157,31 +157,31 @@ while i == 1:
     inp = input("Choose action: ")
 #DONE1-balance: input +- acc_val; add acc_val to audit
     if inp == "1" :
-        balance(acc_val)
+        manager.acc_val = manager.execute("balance", manager.acc_val, manager.audit)
 #DONE2-sale: input product, price, ammount; check: if product in stock,
 #remove product ammount from stock, add price to acc_val; add to audit
 # negative value check, remove items from stock, add income to acc_val
     elif inp == "2":
-        sale(acc_val)
-#DONE3-buy: input product, price, ammount; check if prduct in stock, add if not
+        manager.execute("sale", manager.acc_val, manager.stock, manager.audit)
+    #DONE3-buy: input product, price, ammount; check if prduct in stock, add if not
 #add product to stock, substrackt price from acc_val, check if acc_val is negative
 #add to audit, negative value check, substract costs from acc_val
     elif inp == "3":
-        buy(acc_val)
+        manager.execute("buy", manager.acc_val, manager.stock, manager.audit)
 #DONE4-print acc_val
     elif inp == "4":
-        print_acc_val(acc_val)
+        manager.execute("account", manager.acc_val)
 #DONE5-list stock: print product, price, ammount for evry item in stock
     elif inp == "5":
-        list_stock(stock)
+        manager.execute("stock", manager.stock)
 
 #DONE6-list item: input product; print ammount for input
     elif inp == "6":
-        list_item()
+        manager.execute("item", manager.stock)
 #DONE7-audit: input from, to; print recorded actions with index from-to: add range
     elif inp == "7":
-        audit_func()
+        manager.execute("audit", manager.audit)
 #DONE8-break
     elif inp == "8":
-        exit()
+        manager.execute("exit", manager.acc_val, manager.stock, manager.audit)
         break
